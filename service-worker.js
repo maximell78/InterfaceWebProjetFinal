ff1b871d358d7782684928ae68e73e6d6d7eb988
maximell78 <maximell78@users.noreply.github.com>
2021-08-21 12:04:23 -0400

    Installation du PWA

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 //Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v3.1';
+const CACHE_NAME = 'static-cache-v4';
 
 self.addEventListener('activate', (evt) => {
    console.log('[ServiceWorker] Activate');
