c4ab0be862ae34aafafbeb9b920e6ce904350d47
maximell78 <maximell78@users.noreply.github.com>
2021-08-21 11:43:02 -0400

    MaJ offline

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,32 @@
+//Update cache names any time any of the cached files change.
+const CACHE_NAME = 'static-cache-v2';
+
+self.addEventListener('activate', (evt) => {
+   console.log('[ServiceWorker] Activate');
+
+   //Remove previous cached data from disk.
+   evt.waitUntil(
+       caches.keys().then((keyList) => {
+           return Promise.all(keyList.map((key) => {
+               if (key !== CACHE_NAME) {
+                   console.log('[ServiceWorker] Removing old cache', key);
+                   return caches.delete(key);
+               }
+           }));
+       })
+   );
+
+   self.clients.claim();
+});
+
 //Add list of files to cache here.
 const FILES_TO_CACHE = [
     'offline.html',
+    'bieres.html',
+    'contact.html',
+    'critiques.html',
+    'index.html',
+    'merci.html'
  ];
  
  
@@ -14,4 +40,23 @@ const FILES_TO_CACHE = [
         })
     );
     self.skipWaiting();
+ });
+
+ 
+self.addEventListener('fetch', (evt) => {
+    console.log('[ServiceWorker] Fetch', evt.request.url);
+    //Add fetch event handler here.
+    if (evt.request.mode !== 'navigate') {
+        // Not a page navigation, bail.
+        return;
+    }
+    evt.respondWith(
+        fetch(evt.request)
+            .catch(() => {
+                return caches.open(CACHE_NAME)
+                    .then((cache) => {
+                        return cache.match('https://maximell78.github.io/InterfaceWebProjetFinal/offline.html');
+                    });
+            })
+    );
  }); 
